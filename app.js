3e43e0336af0c0726a780c047b28cd9d07bd1857
Bill <info@billgr17.click>
2017-09-18 14:01:01 +0300

    changed appjs moved styl directory to dev
    All the javascript - stylus files will be on dev instead of public
    
    public files will always be compressed and not editable

--- app.js
+++ app.js
@@ -21,7 +21,13 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(stylus.middleware({
+  src: path.join(__dirname, 'dev'),
+	dest: path.join(__dirname, 'public'),
+   compile: function compile(str, path) { //this will compress the css and remove all comments
+      return stylus(str).set('filename', path).set('compress', true);
+}
+ }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
